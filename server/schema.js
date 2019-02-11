8d169cb0fe21bbe20a9f6e0aec39e03f53498709
Travis Lang <tlang50@icloud.com>
2019-02-10 18:13:43 -0600

    add movie api to graphql server

--- server/schema.js
+++ server/schema.js
@@ -7,9 +7,18 @@ module.exports = gql`
         name: String!
     }
 
+    type Movie {
+        name: String!
+        posterPath: String!
+        genres: [Genre!]!
+        overview: String!
+        releaseDate: String!
+    }
+
     type Query {
         genre(id: Int!): Genre!
         getGenres: [Genre!]!
+        getPopularMovies: [Movie!]!
     }
 `;
 
